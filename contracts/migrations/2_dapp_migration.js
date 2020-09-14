5023f68dd0bcc05e3da4a0c7c77a6d8d3845ec6a
Shivva <matthieumariejoseph@gmail.com>
2020-09-15 01:41:22 +0200

    Migrate all smart contract in 2_dapp_migration.js

--- contracts/migrations/2_dapp_migration.js
+++ contracts/migrations/2_dapp_migration.js
@@ -5,7 +5,7 @@ const GelatoCoreExtension = artifacts.require("GelatoCoreExtension");
 
 module.exports = async function (deployer) {
   await deployer.deploy(GelatoAction);
-  // await deployer.deploy(GelatoCondition, '0x733aDEf4f8346FD96107d8d6605eA9ab5645d632');
-  // await deployer.deploy(Executor, '0x733aDEf4f8346FD96107d8d6605eA9ab5645d632'); // 0xDd916aa9AcD456Ec30Cd88402f3E349319BED9bf
-  // await deployer.deploy(GelatoCoreExtension, '0x733aDEf4f8346FD96107d8d6605eA9ab5645d632');
+  await deployer.deploy(GelatoCondition, '0x733aDEf4f8346FD96107d8d6605eA9ab5645d632');
+  await deployer.deploy(Executor, '0x733aDEf4f8346FD96107d8d6605eA9ab5645d632');
+  await deployer.deploy(GelatoCoreExtension, '0x733aDEf4f8346FD96107d8d6605eA9ab5645d632');
 };
